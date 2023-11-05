d18ac8a5bb249a81852618a6446e7524c518bf3a
kkamjjing2 <kkamjjing2@gmail.com>
2023-11-06 04:54:20 +0900

    [ADD] - Board(Entity) 추가

--- src/main/java/com/kkamjjing/boardserver/entity/Board.java
+++ src/main/java/com/kkamjjing/boardserver/entity/Board.java
@@ -1,5 +1,57 @@
 package com.kkamjjing.boardserver.entity;
 
-public class Board {
+import com.kkamjjing.boardserver.dto.BoardRequestDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String createAt;
+
+    public Board (String title, String name, String password, String content, String createAt) {
+        this.title = title;
+        this.name = name;
+        this.password = password;
+        this.content = content;
+        this.createAt = createAt;
+    }
+
+    public void update(BoardRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.name = requestDto.getName();
+        this.password = requestDto.getPassword();
+        this.content = requestDto.getContent();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
