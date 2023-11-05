799b8499c3273a1847d5fc67960bdc800112f6bd
kkamjjing2 <kkamjjing2@gmail.com>
2023-11-06 04:54:03 +0900

    [ADD] - BoardRequestDto 추가

---
+++ src/main/java/com/kkamjjing/boardserver/dto/BoardRequestDto.java
@@ -0,0 +1,15 @@
+package com.kkamjjing.boardserver.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardRequestDto {
+    private String title;
+    private String name;
+    private String password;
+    private String content;
+}
