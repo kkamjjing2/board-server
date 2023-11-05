63059b18750ca7c1418a934d81991e23fce9c06f
kkamjjing2 <kkamjjing2@gmail.com>
2023-11-06 05:54:35 +0900

    [ADD] - BoardRepository 추가

--- src/main/java/com/kkamjjing/boardserver/repository/BoardRepository.java
+++ src/main/java/com/kkamjjing/boardserver/repository/BoardRepository.java
@@ -1,4 +1,7 @@
 package com.kkamjjing.boardserver.repository;
 
-public class BoardRepository {
+import com.kkamjjing.boardserver.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
